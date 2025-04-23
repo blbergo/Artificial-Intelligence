3a3a5ac97f0f8f41b226abdd784e1a3260427dc4
blbergo <bryan.l.bergo@gmail.com>
2025-04-23 15:49:41 -0700

    feat: working  mlp

--- assignment-2/train.py
+++ assignment-2/train.py
@@ -1,8 +1,9 @@
 from torchrl.envs import GymEnv
 import gym
 from torchviz import make_dot
-from torchrl.modules import Actor, MLP, ValueOperator
-from torchrl.objectives import DDPGLoss
+from torchrl.modules import ProbabilisticActor, MLP
+from tensordict.nn import TensorDictModule
+import torch.distributions as dist
 
 gym.register(
     id="Chess-v0",
@@ -15,13 +16,22 @@ env = GymEnv("Chess-v0")
 n_obs = env.observation_spec["observation"].shape[-1]
 n_act = env.action_spec.n
 
-actor = Actor(MLP(in_features=n_obs, out_features=n_act), in_keys=["observation"], out_keys=["action"])
-#value_net = ValueOperator(
- #   MLP(in_features=n_obs + n_act, out_features=1),
-  #  in_keys=["observation", "action"],
-#)
+actor_mlp = MLP(
+    in_features=n_obs,
+    out_features=n_act,
+)
 
-#ddpg_loss = DDPGLoss(actor_network=actor, value_network=value_net)
+actor_module = TensorDictModule(
+    actor_mlp,
+    in_keys=["observation"],
+    out_keys=["logits"],
+)
+
+actor = ProbabilisticActor(
+    module=actor_module,
+    distribution_class=dist.Categorical,
+    in_keys=["logits"],
+)
+
+
 rollout = env.rollout(max_steps=100, policy=actor)
-#loss_vals = ddpg_loss(rollout)
-#print(loss_vals)
