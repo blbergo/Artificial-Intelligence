5c80267632aa197f13bca74783d7a9d5f5774fdf
blbergo <bryan.l.bergo@gmail.com>
2025-04-23 14:09:01 -0700

    feat: sync

--- assignment-2/train.py
+++ assignment-2/train.py
@@ -13,17 +13,15 @@ gym.register(
 env = GymEnv("Chess-v0")
 
 n_obs = env.observation_spec["observation"].shape[-1]
-n_act = env.action_spec.shape[-1]
+n_act = env.action_spec.n
 
-print(env.action_spec)
+actor = Actor(MLP(in_features=n_obs, out_features=n_act), in_keys=["observation"], out_keys=["action"])
+#value_net = ValueOperator(
+ #   MLP(in_features=n_obs + n_act, out_features=1),
+  #  in_keys=["observation", "action"],
+#)
 
-actor = Actor(MLP(in_features=n_obs, out_features=n_act, num_cells=[32, 32]))
-value_net = ValueOperator(
-    MLP(in_features=n_obs + n_act, out_features=1, num_cells=[32, 32]),
-    in_keys=["observation", "action"],
-)
-
-ddpg_loss = DDPGLoss(actor_network=actor, value_network=value_net)
+#ddpg_loss = DDPGLoss(actor_network=actor, value_network=value_net)
 rollout = env.rollout(max_steps=100, policy=actor)
 #loss_vals = ddpg_loss(rollout)
 #print(loss_vals)
